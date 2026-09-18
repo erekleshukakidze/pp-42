49bc6f9016721121c103304df92900990ffe840d
erekleshukakidze <erekleshukakidze2009@gmail.com>
2026-09-18 20:42:01 +0400

    first commit

---
+++ lecture2/variables_and_types.py
@@ -0,0 +1,9 @@
+student_name = "Erekle"          #str
+student_age = 18                 #int
+user_bodyweight_kg = 72.5        #float
+is_responsibility_bearing = True #bool
+
+print("student_name",student_name, type(student_name))
+print("student_age",student_age, type(student_age))
+print("user_bodyweight_kg", user_bodyweight_kg, type(user_bodyweight_kg))
+# print("is_responsibility_bearing",is_responsibility_bearing, type(is_responsibility_bearing))
